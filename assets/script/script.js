d0b4ea5a229e7ec246532e337c36c975ef6a4dad
Dereemii <leshermira@gmail.com>
2020-08-30 22:11:09 -0400

    agrego grafico

--- assets/script/script.js
+++ assets/script/script.js
@@ -38,11 +38,40 @@ generarCard =(personaje) =>{
                 <div>Tipo: ${personaje.types[0].type.name}</div>
                 <div>Peso: ${personaje.weight}</div>
                 <div>Altura: ${personaje.height}</div>
-            </span>   
-            
-            
-            `
-            ;
+            </span>   `
+                //Grafico CANVAS
+    var hp =  personaje.stats[0].base_stat;
+    var atk = personaje.stats[1].base_stat;
+    var def = personaje.stats[2].base_stat;
+    var sat = personaje.stats[3].base_stat;
+    var sdf = personaje.stats[4].base_stat;
+    var spd = personaje.stats[5].base_stat;
+    var pokest = [];
+     pokest.push(hp, atk, def, sat, sdf, spd);
+
+
+      var chart = new CanvasJS.Chart("pokemon-grafico", { 
+        theme: "light1", // "light2", "dark1", "dark2"
+        animationEnabled: false, // change to true		
+        title:{
+          text: "Estadisticas Pokémon"
+        },
+        data: [
+        {
+          // Change type to "bar", "area", "spline", "pie",etc.
+          type: "column",
+          dataPoints: [
+            { label: "HP",  y:parseFloat(hp)  },
+            { label: "Ataque", y: parseFloat(atk)  },
+            { label: "Defensa", y: parseFloat(def) },
+            { label: "Ataque Especial",  y: parseFloat(sat) },
+            { label: "Defensa Especial",  y: parseFloat(sdf)  },
+            { label: "Velocidad",  y: parseFloat(spd)  }
+          ]
+        }
+        ]
+      });
+      chart.render();
   return pokecard; 
 }
 
